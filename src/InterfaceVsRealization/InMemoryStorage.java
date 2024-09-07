ad192e0035c7c1d0cd3d6990634054a68fbb5a10
zrdpsh <zerdposh@gmail.com>
2024-09-07 06:01:56 +0300

    In-memory realization added

---
+++ src/InterfaceVsRealization/InMemoryStorage.java
@@ -0,0 +1,19 @@
+package InterfaceVsRealization;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class InMemoryStorage implements Storage {
+    private Map<Integer, String> storage = new HashMap<>();
+    private int counter = 0;
+
+    @Override
+    public void save(String data) {
+        storage.put(counter++, data);
+    }
+
+    @Override
+    public String retrieve(int id) {
+        return storage.get(id);
+    }
+}
