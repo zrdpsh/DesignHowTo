c158b67dad6bce9197232e262d1e2caa85cb09ce
zrdpsh <zerdposh@gmail.com>
2024-09-14 00:37:44 +0300

    Random tests added

--- src/AverageCalculator/Main.java
+++ src/AverageCalculator/Main.java
@@ -3,7 +3,9 @@ package AverageCalculator;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Returns Nan without an error");
+        System.out.println("Without tests:");
+
+        System.out.println("1. Method will divide by zero without any complain");
         int[] aEmptyArray = new int[0];
         AverageCalculator aCalculator = new AverageCalculator();
         for (int i = 0; i < aEmptyArray.length; i++) System.out.println(aEmptyArray[i]);
