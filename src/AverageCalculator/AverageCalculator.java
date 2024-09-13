4eeb593bd53bb32f7d73be824ef684fef35d479e
zrdpsh <zerdposh@gmail.com>
2024-09-13 23:10:36 +0300

    Average calculator class and method added

---
+++ src/AverageCalculator/AverageCalculator.java
@@ -0,0 +1,11 @@
+package AverageCalculator;
+
+public class AverageCalculator {
+    public double calculateAverage(int[] numbers) {
+        double sum = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            sum +=  numbers[i];
+        }
+        return sum / numbers.length;
+    }
+}
