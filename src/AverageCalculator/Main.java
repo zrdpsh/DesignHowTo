d2f4d4748de4510642b790209c117dac858c7e49
zrdpsh <zerdposh@gmail.com>
2024-09-13 23:25:06 +0300

    "Returns Nan without an error" added

--- src/AverageCalculator/Main.java
+++ src/AverageCalculator/Main.java
@@ -2,8 +2,11 @@ package AverageCalculator;
 
 public class Main {
     public static void main(String[] args) {
-        int[] aEmptyArray = new int[12];
+
+        System.out.println("Returns Nan without an error");
+        int[] aEmptyArray = new int[0];
         AverageCalculator aCalculator = new AverageCalculator();
+        for (int i = 0; i < aEmptyArray.length; i++) System.out.println(aEmptyArray[i]);
         System.out.println(aCalculator.calculateAverage(aEmptyArray));
     }
 }
