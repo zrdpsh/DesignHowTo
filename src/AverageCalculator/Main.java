8f7f5fb151d4b999a03a9734ce6a840edbf7f471
zrdpsh <zerdposh@gmail.com>
2024-09-13 23:20:00 +0300

    Main class added

--- src/AverageCalculator/Main.java
+++ src/AverageCalculator/Main.java
@@ -5,5 +5,6 @@ public class Main {
         int[] aEmptyArray = new int[12];
         AverageCalculator aCalculator = new AverageCalculator();
         System.out.println(aCalculator.calculateAverage(aEmptyArray));
+
     }
 }
