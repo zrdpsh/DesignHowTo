44f62b87f8e4ed87e2e7d9ffd09735a80ec503bf
zrdpsh <zerdposh@gmail.com>
2024-09-15 05:03:30 +0300

    Main method rewritten

--- src/AverageCalculator/Main.java
+++ src/AverageCalculator/Main.java
@@ -1,17 +1,19 @@
 package AverageCalculator;
 
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Without tests:");
+        System.out.println("The floating-point arithmetic doesn't give the accurate results");
 
-        System.out.println("1. Method will divide by zero without any complain");
-        int[] aEmptyArray = new int[0];
         AverageCalculator aCalculator = new AverageCalculator();
-        for (int i = 0; i < aEmptyArray.length; i++) System.out.println(aEmptyArray[i]);
-        System.out.println(aCalculator.calculateAverage(aEmptyArray));
-
-
-        
+        int[] numbers = {1, 1, 1, 1000000};
+        double result = aCalculator.calculateAverage(numbers);
+        assertEquals(250_000.0, result, 1);
+        assertNotEquals(250_000.0, result);
     }
 }
