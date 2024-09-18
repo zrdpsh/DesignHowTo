11d94f98cd32355d8440e7f87072597a086b51fd
zrdpsh <zerdposh@gmail.com>
2024-09-18 16:30:13 +0300

    typos fixed

--- src/GradeCalculator/GradeCalculatorTest.java
+++ src/GradeCalculator/GradeCalculatorTest.java
@@ -1,21 +1,17 @@
 package GradeCalculator;
 
-import AverageCalculator.AverageCalculator;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
-import java.util.Random;
 
 import java.util.*;
-import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class GradeCalculatorTest {
 
     @Test
-    public void testNullGradesList() {
+    public void testNullGrades() {
         GradeCalculator calculator = new GradeCalculator();
         List<Integer> grades = null;
         double result = calculator.calculateAverage(grades);
@@ -23,7 +19,7 @@ public class GradeCalculatorTest {
     }
 
     @Test
-    public void testEmptyGradesList() {
+    public void testEmptyGrades() {
         GradeCalculator calculator = new GradeCalculator();
         List<Integer> grades = List.of();
         double result = calculator.calculateAverage(grades);
@@ -31,7 +27,7 @@ public class GradeCalculatorTest {
     }
 
     @Test
-    public void testAllNullGradesList() {
+    public void testAllNullGrades() {
         GradeCalculator calculator = new GradeCalculator();
         List<Integer> grades = Arrays.asList(null, null, null);
         double result = calculator.calculateAverage(grades);
@@ -39,11 +35,11 @@ public class GradeCalculatorTest {
     }
 
     @Test
-    public void testMixedNullAndNonNullGrades() {
+    public void testMixedNullAndNonNull() {
         GradeCalculator calculator = new GradeCalculator();
-        List<Integer> grades = Arrays.asList(90, null, 80, null, 70);
+        List<Integer> grades = Arrays.asList(190, null, 180, null, 170);
         double result = calculator.calculateAverage(grades);
-        assertEquals(80.0, result, 0.01);
+        assertEquals(180.0, result, 0.01);
     }
 
     @Test
