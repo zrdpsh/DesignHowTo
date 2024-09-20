674454f7e3a9175114334a1d4443a9474a6f2b1e
zrdpsh <149115105+zrdpsh@users.noreply.github.com>
2024-09-20 21:13:38 +0300

    Update GradeCalculatorTest.java

--- src/GradeCalculator/GradeCalculatorTest.java
+++ src/GradeCalculator/GradeCalculatorTest.java
@@ -1,11 +1,8 @@
 package GradeCalculator;
 
 import org.junit.jupiter.api.Test;
-
 import java.util.Arrays;
-
 import java.util.*;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 public class GradeCalculatorTest {
