5c8216d3a007eb4dc9cf4b86a3a5601db7b35495
zrdpsh <149115105+zrdpsh@users.noreply.github.com>
2024-10-03 21:48:13 +0300

    Update GradeCalculatorTest.java

--- src/GradeCalculator/GradeCalculatorTest.java
+++ src/GradeCalculator/GradeCalculatorTest.java
@@ -7,6 +7,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class GradeCalculatorTest {
 
+    
     @Test
     public void testNullGrades() {
         GradeCalculator calculator = new GradeCalculator();
