1684eb1bbcf68b474b8300b6f8f643abdbf1a834
zrdpsh <149115105+zrdpsh@users.noreply.github.com>
2024-10-18 21:10:38 +0300

    Update FileWriterMock.java

--- src/MultithreadingSolutions/FileWriterMock.java
+++ src/MultithreadingSolutions/FileWriterMock.java
@@ -16,6 +16,7 @@ public class FileWriterMock {
         writer = new BufferedWriter(new FileWriter(fileName, true)); // Open file in append mode
         System.out.println(Thread.currentThread().getName() + " opened the file.");
     }
+    
 
     public void write(String content) throws IOException {
         if (writer == null) {
