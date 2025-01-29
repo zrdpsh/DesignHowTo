7f5196a641dca0ce97a23ebfe4ae1001204b1f9f
zrdpsh <149115105+zrdpsh@users.noreply.github.com>
2025-01-29 19:40:14 +0300

    Update GradeCalculator.java

--- src/GradeCalculator/GradeCalculator.java
+++ src/GradeCalculator/GradeCalculator.java
@@ -4,8 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-
-
 public class GradeCalculator {
 
     public static double calculateAverage(List<Integer> grades) {
