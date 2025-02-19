61b576743445b31f892295fb3604915ead588b96
zrdpsh <149115105+zrdpsh@users.noreply.github.com>
2025-02-19 21:21:23 +0300

    Update InMemoryStorage.java

--- src/InterfaceVsRealization/InMemoryStorage.java
+++ src/InterfaceVsRealization/InMemoryStorage.java
@@ -7,7 +7,8 @@ public class InMemoryStorage implements Storage {
     private Map<Integer, String> storage = new HashMap<>();
     private int counter = 0;
    
-   
+
+    
     @Override
     public void save(String data) {
         storage.put(counter++, data);
